addedc3784d6621fa0346f57a610e852a2d1525c
Ribamar Marcos José dos Santos <ribamarmjs@MacBook-Pro-de-Ribamar.local>
2016-12-03 13:30:29 -0300

    criando layout detail inicio

--- PROJETO_IGARASSU/Resources/android/alloy/controllers/detailPlace.js
+++ PROJETO_IGARASSU/Resources/android/alloy/controllers/detailPlace.js
@@ -8,10 +8,10 @@ function __processArg(obj, key) {
 }
 
 function Controller() {
-    function __alloyId9(e) {
+    function __alloyId13(e) {
         if (e && e.fromAdapter) return;
-        __alloyId9.opts || {};
-        var models = filterPlace(__alloyId8);
+        __alloyId13.opts || {};
+        var models = filterPlace(__alloyId12);
         var len = models.length;
         var rows = [];
         for (var i = 0; len > i; i++) {
@@ -19,22 +19,45 @@ function Controller() {
             __alloyId0.__transform = _.isFunction(__alloyId0.transform) ? __alloyId0.transform() : __alloyId0.toJSON();
             var __alloyId2 = Ti.UI.createTableViewRow({});
             rows.push(__alloyId2);
-            var __alloyId3 = Ti.UI.createView({});
+            var __alloyId3 = Ti.UI.createView({
+                layout: "vertical"
+            });
             __alloyId2.add(__alloyId3);
             var __alloyId4 = Ti.UI.createImageView({
+                width: Ti.UI.FILL,
+                height: 250,
+                borderWidth: 5,
                 image: __alloyId0.__transform.photo
             });
             __alloyId3.add(__alloyId4);
-            var __alloyId5 = Ti.UI.createView({});
+            var __alloyId5 = Ti.UI.createImageView({
+                image: "/favorite.png",
+                width: Ti.UI.SIZE,
+                height: Ti.UI.SIZE
+            });
             __alloyId3.add(__alloyId5);
-            var __alloyId6 = Ti.UI.createLabel({
+            var __alloyId6 = Ti.UI.createImageView({});
+            __alloyId3.add(__alloyId6);
+            var __alloyId7 = Ti.UI.createLabel({
                 text: __alloyId0.__transform.place_name
             });
-            __alloyId5.add(__alloyId6);
-            var __alloyId7 = Ti.UI.createLabel({
+            __alloyId3.add(__alloyId7);
+            var __alloyId8 = Ti.UI.createLabel({
+                text: __alloyId0.__transform.price
+            });
+            __alloyId3.add(__alloyId8);
+            var __alloyId9 = Ti.UI.createLabel({
+                text: __alloyId0.__transform.address
+            });
+            __alloyId3.add(__alloyId9);
+            var __alloyId10 = Ti.UI.createLabel({
                 text: __alloyId0.__transform.phone
             });
-            __alloyId5.add(__alloyId7);
+            __alloyId3.add(__alloyId10);
+            var __alloyId11 = Ti.UI.createLabel({
+                text: __alloyId0.__transform.description_eng
+            });
+            __alloyId3.add(__alloyId11);
         }
         $.__views.tableViewDetailPlace.setData(rows);
     }
@@ -61,23 +84,24 @@ function Controller() {
     var $ = this;
     var exports = {};
     var __defers = {};
-    $.__views.detailPlace = Ti.UI.createWindow({
-        id: "detailPlace"
+    $.__views.winDetail = Ti.UI.createWindow({
+        layout: "vertical",
+        id: "winDetail"
     });
-    $.__views.detailPlace && $.addTopLevelView($.__views.detailPlace);
+    $.__views.winDetail && $.addTopLevelView($.__views.winDetail);
     $.__views.lblPlace = Ti.UI.createLabel({
         id: "lblPlace"
     });
-    $.__views.detailPlace.add($.__views.lblPlace);
+    $.__views.winDetail.add($.__views.lblPlace);
     $.__views.tableViewDetailPlace = Ti.UI.createTableView({
         id: "tableViewDetailPlace"
     });
-    $.__views.detailPlace.add($.__views.tableViewDetailPlace);
-    var __alloyId8 = Alloy.Collections["places"] || places;
-    __alloyId8.on("fetch destroy change add remove reset", __alloyId9);
+    $.__views.winDetail.add($.__views.tableViewDetailPlace);
+    var __alloyId12 = Alloy.Collections["places"] || places;
+    __alloyId12.on("fetch destroy change add remove reset", __alloyId13);
     showDescriptionPlace ? $.addListener($.__views.tableViewDetailPlace, "click", showDescriptionPlace) : __defers["$.__views.tableViewDetailPlace!click!showDescriptionPlace"] = true;
     exports.destroy = function() {
-        __alloyId8 && __alloyId8.off("fetch destroy change add remove reset", __alloyId9);
+        __alloyId12 && __alloyId12.off("fetch destroy change add remove reset", __alloyId13);
     };
     _.extend($, $.__views);
     var row = $.args;
