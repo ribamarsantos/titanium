e89c9fa34dab4d2397f5854810ae7ce2405bac1c
Ribamar <ribamaar@gmail.com>
2016-12-02 22:34:58 -0300

    removendo de novo

--- PROJETO_IGARASSU/Resources/android/app.js
+++
@@ -1,7 +0,0 @@
-var Alloy = require("alloy"), _ = Alloy._, Backbone = Alloy.Backbone;
-
-Alloy.Collections.temperature = new Backbone.Collection();
-
-Alloy.Collections.places = Alloy.createCollection("place");
-
-Alloy.createController("index");
